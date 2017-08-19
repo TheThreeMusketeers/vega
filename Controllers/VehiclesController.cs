d812f98d5be549c7ce225a4fea3b0c8d809b04a1
ersinsivaz <ersinsivaz@hotmail.com>
2017-08-19 19:32:35 +0300

    5. bölümde kaldım

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using vega.Controllers.Resources;
 using vega.Models;
 using vega.Persistence;
@@ -23,6 +24,9 @@ namespace vega.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -32,6 +36,57 @@ namespace vega.Controllers
 
             var result = mapper.Map<Vehicle,VehicleResource>(vehicle);
             return Ok(result);
-        }
+        }//CreateVehicle
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id,[FromBody] VehicleResource vehicleResource)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles
+                .Include(vf=>vf.Features)
+                .SingleOrDefaultAsync(v=>v.Id==id);
+
+            if(vehicle==null)
+                return NotFound();
+
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource,vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle,VehicleResource>(vehicle);
+            return Ok(result);
+        }//UpdateVehicle
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if(vehicle==null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }//DeleteVehicle
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles
+            .Include(v=>v.Features)
+            .SingleOrDefaultAsync(v=>v.Id==id);
+
+            if(vehicle==null)
+                return NotFound();
+
+            var vehicleResource = mapper.Map<Vehicle,VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
+        }//DeleteVehicle
     }//cs
 }//ns
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using vega.Controllers.Resources;
 using vega.Models;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace vega.Mapping
 {
@@ -19,10 +20,45 @@ namespace vega.Mapping
 
             //mapping API resources to domain
             CreateMap<VehicleResource,Vehicle>()
+                .ForMember(v=>v.Id,opt=>opt.Ignore())
                 .ForMember(v=>v.ContactName,opt=>opt.MapFrom(vr=>vr.Contact.Name))
                 .ForMember(v=>v.ContactEmail,opt=>opt.MapFrom(vr=>vr.Contact.Email))
                 .ForMember(v=>v.ContactPhone,opt=>opt.MapFrom(vr=>vr.Contact.Phone))
-                .ForMember(v=>v.Features,opt=>opt.MapFrom(vr=>vr.Features.Select(id=>new VehicleFeature {FeatureId=id})));
+                //.ForMember(v=>v.Features,opt=>opt.MapFrom(vr=>vr.Features.Select(id=>new VehicleFeature {FeatureId=id})));
+                .ForMember(v=>v.Features,opt=>opt.Ignore())
+                .AfterMap((vr,v)=>{
+                    //Remove unselected features
+                    /* 
+                    var removedFeatures=new List<VehicleFeature>();
+                    foreach(var f in v.Features)
+                    {
+                        if(!vr.Features.Contains(f.FeatureId))
+                            removedFeatures.Add(f);
+                    }
+                    */
+
+                    var removedFeatures = v.Features.Where(f=>!vr.Features.Contains(f.FeatureId));
+
+                    foreach (var f in removedFeatures)
+                    {
+                        v.Features.Remove(f);
+                    }
+
+                    //add new features
+                    /* 
+                    foreach (var id in vr.Features)
+                    {
+                        if(!v.Features.Any(f=>f.FeatureId==id))
+                            v.Features.Add(new VehicleFeature{FeatureId=id});
+                    }
+                    */
+
+                    var addedFeatures = vr.Features.Where(id=>!v.Features.Any(f=>f.FeatureId==id));
+                    foreach (var id in addedFeatures)
+                    {
+                        v.Features.Add(new VehicleFeature{FeatureId=id});
+                    }
+                });
         }
     }
 }
