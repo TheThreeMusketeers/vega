d6f50cb5f5bfee32913d61efad1ef548b44362f1
ersin sivaz <ersinsivaz@hotmail.com>
2017-08-29 17:21:08 +0300

    Ders 103 de kaldım

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -24,7 +24,7 @@ namespace vega.Mapping
                 .ForMember(vr=>vr.Features,opt=>opt.MapFrom(v=>v.Features.Select(vf=>new KeyValuePairResource{Id=vf.Feature.Id,Name=vf.Feature.Name})));
             
             //mapping API resources to domain
-            CreateMap<FilterResource,Filter>();
+            CreateMap<VehicleQueryResource,VehicleQuery>();
             CreateMap<SaveVehicleResource,Vehicle>()
                 .ForMember(v=>v.Id,opt=>opt.Ignore())
                 .ForMember(v=>v.ContactName,opt=>opt.MapFrom(vr=>vr.Contact.Name))
