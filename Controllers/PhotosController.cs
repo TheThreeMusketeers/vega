cd37f9686529f1641dd4c6ef9658f749b25915d3
ersinsivaz <ersinsivaz@hotmail.com>
2017-09-02 15:35:01 +0300

    Ders 122 de kaldım(photo upload)

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Core;
+using vega.Core.Models;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles/{vehicleId}/photos")]
+    public class PhotosController : Controller
+    {
+        private readonly IHostingEnvironment host;
+        private readonly IVehicleRepository repository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+        private readonly int MAX_BYTES = 10 * 1024 * 1024;
+        private readonly string[] ACCEPTED_FILE_TYPES = new []{".jpg",".jpeg",".png"};
+
+        public PhotosController(
+            IHostingEnvironment host, 
+            IVehicleRepository repository,
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+            this.host = host;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
+        {
+            var vehicle = await repository.GetVehicle(vehicleId,includeRelated:false);
+            if(vehicle == null)
+                return NotFound();
+
+            if(file == null) return BadRequest("File is null");
+            if(file.Length == 0) return BadRequest("File is empty");
+            if(file.Length > MAX_BYTES) return BadRequest("Maximum file size is excedeed"); 
+            if(!ACCEPTED_FILE_TYPES.Any(s=>s == Path.GetExtension(file.FileName))) return BadRequest("Invalid file type");
+
+            var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
+            if (!Directory.Exists(uploadsFolderPath))
+                Directory.CreateDirectory(uploadsFolderPath);
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine(uploadsFolderPath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var photo = new Photo {FileName = fileName};
+            vehicle.Photos.Add(photo);
+
+            await unitOfWork.CompleteAsync();
+
+            return Ok(mapper.Map<Photo,PhotoResource>(photo));
+        }
+    }//cs
+}//ns
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -11,6 +11,7 @@ namespace vega.Mapping
         public MappingProfile()
         {
             //mapping domain to API resources
+            CreateMap<Photo,PhotoResource>();
             CreateMap(typeof(QueryResult<>),typeof(QueryResultResource<>));
             CreateMap<Make,MakeResource>();
             CreateMap<Make,KeyValuePairResource>();
